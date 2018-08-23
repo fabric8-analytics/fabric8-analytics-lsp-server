50fd034d76b0f3ed9753b4d905bef118f4a968ff
Jyasveer Gotta <jyasveer@gmail.com>
2018-08-23 16:45:58 +0530

    feat(cicd): Files to bring in cicd process

---
+++ cico_release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Show command before executing
+set -x
+
+# Exit on error
+set -e
+
+# This option sets the exit code of a pipeline to that of the rightmost command to exit with a
+# non-zero status, or to zero if all commands of the pipeline exit successfully.
+set -o pipefail
+
+function release() {
+    # add .npmrc file to give permissions
+    echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > ~/.npmrc
+
+    npm whoami
+
+    # Enable verbose output
+    npm config set loglevel verbose
+
+    # Build and Release fabric8-analytics-lsp-server (It will update the tag on github and push fabric8-analytics-lsp-server to npmjs.org)
+    npm run semantic-release
+}
---
+++ cico_setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -ex
+
+load_jenkins_vars() {
+  if [ -e "jenkins-env" ]; then
+      cat jenkins-env \
+        | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId|RECOMMENDER_API_TOKEN|NPM_TOKEN|GH_TOKEN)=" \
+        | sed 's/^/export /g' \
+        > ~/.jenkins-env
+      source ~/.jenkins-env
+
+      echo "CICO: Jenkins environment variables loaded"
+  fi
+}
+
+prep() {
+  yum -y update
+  yum -y install make git gcc-c++ bzip2 fontconfig
+  curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
+  yum -y install nodejs
+}
+
+install_dependencies() {
+  # clean up
+  rm -Rf ca-lsp-server.tar output/
+
+  # Build fabric8-analytics-stack-reports-ui
+  npm install;
+
+  if [ $? -eq 0 ]; then
+      echo 'CICO: npm install : OK'
+  else
+      echo 'CICO: npm install : FAIL'
+      exit 1
+  fi
+}
+
+build_project() {
+  # run build
+  npm run build
+
+  # copy static files to output folder
+  cp LICENSE package.json README.md output
+
+  if [ $? -eq 0 ]; then
+    echo 'CICO: build OK'
+  else
+    echo 'CICO: build FAIL'
+    exit 1
+  fi
+}
+
+. cico_release.sh
+
+load_jenkins_vars
+
+prep
