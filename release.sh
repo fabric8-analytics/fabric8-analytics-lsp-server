f08ac41ecfa4370a92475a582acd1a16d8bfbb5d
Jyasveer Gotta <jyasveer@gmail.com>
2018-08-31 15:47:21 +0530

    fix(publish): publish tar

--- release.sh
+++
@@ -1,9 +0,0 @@
-res=$(curl -s  https://api.github.com/repos/fabric8-analytics/fabric8-analytics-lsp-server/releases/latest | jq '. | { id: .id, upload_url: .upload_url }')
-
-id=$(echo $res | jq .id)
-upload_url=$(echo $res | jq .upload_url) 
-
-echo $id
-echo $upload_url
-
-ls
