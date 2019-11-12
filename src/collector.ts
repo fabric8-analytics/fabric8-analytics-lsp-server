245fc9cef75f677e8afb4b7e1e1885ef3fdad44c
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2019-11-12 11:08:41 +0530

    Refactor send diag message

--- src/collector.ts
+++ src/collector.ts
@@ -5,6 +5,7 @@
 'use strict';
 import { StreamingParser, IPosition, IKeyValueEntry, KeyValueEntry, Variant, ValueType } from './json';
 import * as Xml2Object from 'xml2object';
+import { stream_from_string } from './utils';
 import { Stream } from 'stream';
 
 /* By default the collector is going to process these dependency keys */
@@ -25,7 +26,7 @@ interface IDependency {
 /* Dependency collector interface */
 interface IDependencyCollector {
   classes: Array<string>;
-  collect(file: Stream): Promise<Array<IDependency>>;
+  collect(contents: string): Promise<Array<IDependency>>;
 }
 
 /* Dependency class that can be created from `IKeyValueEntry` */
@@ -51,7 +52,8 @@ class DependencyCollector implements IDependencyCollector {
         this.classes = classes || DefaultClasses
     }
 
-    async collect(file: Stream): Promise<Array<IDependency>> {
+    async collect(contents: string): Promise<Array<IDependency>> {
+        const file = stream_from_string(contents);
         let parser = new StreamingParser(file);
         let dependencies: Array<IDependency> = [];
         let tree = await parser.parse();
@@ -187,7 +189,8 @@ class NaivePomXmlSaxParser {
 class PomXmlDependencyCollector {
     constructor(public classes: Array<string> = ["dependencies"]) {}
 
-    async collect(file: Stream): Promise<Array<IDependency>> {
+    async collect(contents: string): Promise<Array<IDependency>> {
+        const file = stream_from_string(contents);
         let parser = new NaivePomXmlSaxParser(file);
         let dependencies;
          await parser.parse().then(data => {
