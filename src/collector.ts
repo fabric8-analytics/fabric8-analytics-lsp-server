ed5dd8127eb4c14183b6e17ed5f2a9918b55b57e
dgpatelgit <dhpatel@redhat.com>
2020-12-02 14:21:16 +0530

    fix: [APPAI-1650][Golang] Setting bash shell environment while runing manifest command

--- src/collector.ts
+++ src/collector.ts
@@ -180,7 +180,7 @@ class GomodDependencyCollector implements IDependencyCollector {
         let promiseExec = new Promise<Set<string>>((resolve, reject) => {
             const vscodeRootpath = this.manifestFile.replace("file://", "").replace("/go.mod", "")
             exec(getGoLangImportsCmd(),
-                { cwd: vscodeRootpath, maxBuffer: 1024 * 1200 }, (error, stdout, stderr) => {
+                { shell :process.env["SHELL"], windowsHide: true, cwd: vscodeRootpath, maxBuffer: 1024 * 1200 }, (error, stdout, stderr) => {
                 if (error) {
                     if (error.code == 127) { // Invalid command, go executable not found
                         reject(`Unable to locate '${config.golang_executable}'`);
