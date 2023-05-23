2f4e10eecebbcf4eb634cba66d62ac4aeadbea60
Ilona Shishov <Ilona.Shishov@gmail.com>
2023-05-23 10:54:15 +0300

    fix: Collector util test - create map
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>

--- test/collector.test.ts
+++ test/collector.test.ts
@@ -1,18 +1,30 @@
 'use strict';
 
 import { expect } from 'chai';
-import { DependencyMap, SimpleDependency } from '../src/collector';
+import { Dependency, IPosition, ValueType, Variant, KeyValueEntry, DependencyMap, SimpleDependency } from '../src/collector';
 
 describe('Collector util test', () => {
 
-  const deps: Array<SimpleDependency> = [
+  const pos: IPosition = {
+    line: 123,
+    column: 123
+  }; 
+
+  // define manifest dependencies
+  const reqDeps: Array<Dependency> = [
+    new Dependency(new KeyValueEntry('foo', pos, new Variant(ValueType.String, '1.0'), pos)),
+    new Dependency(new KeyValueEntry('bar', pos, new Variant(ValueType.String, '2.0'), pos))
+  ];
+
+  // define api response dependencies
+  const resDeps: Array<SimpleDependency> = [
     new SimpleDependency('foo', '1.0'),
     new SimpleDependency('bar', '2.0')
   ];
 
   it('create map', async () => {
-    const map = new DependencyMap(deps);
-    expect(map.get(deps[0])).to.eql(deps[0]);
-    expect(map.get(deps[1])).to.eql(deps[1]);
+    const map = new DependencyMap(reqDeps);
+    expect(map.get(resDeps[0])).to.eql([reqDeps[0]]);
+    expect(map.get(resDeps[1])).to.eql([reqDeps[1]]);
   });
 })
